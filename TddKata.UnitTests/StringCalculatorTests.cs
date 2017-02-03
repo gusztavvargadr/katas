dedb9af890d0cf649d2b72dbbce58f1737c0643e
Gusztáv Varga <me@gusztavvargadr.me>
2017-02-03 18:35:58 +0000

    Add implementation for empty string

---
+++ TddKata.UnitTests/StringCalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TddKata;
+using Xunit;
+
+namespace TddKata.UnitTests
+{
+    public class StringCalculatorTests
+    {
+        public class Add
+        {
+            [Theory]
+            [InlineData("", 0)]
+            public void ReturnsSum(string numbers, int sum)
+            {
+                var stringCalculator = new StringCalculator();
+
+                var result = stringCalculator.Add(numbers);
+
+                Assert.Equal(sum, result);
+            }
+        }
+    }
+}
---
+++ TddKata/StringCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddKata
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            return 0;
+        }
+    }
+}
