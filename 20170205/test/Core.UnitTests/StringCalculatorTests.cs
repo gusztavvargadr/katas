ceb9ea2003226d33535d66edab5a1c4470b07ce9
Gusztáv Varga <me@gusztavvargadr.me>
2017-02-05 11:46:40 +0000

    Add support for single number

--- 20170205/test/Core.UnitTests/StringCalculatorTests.cs
+++ 20170205/test/Core.UnitTests/StringCalculatorTests.cs
@@ -4,18 +4,36 @@ namespace TddKata.UnitTests
 {
     public class StringCalculatorTests
     {
-        public class Add
+        public class Add : StringCalculatorTests
         {
-            public class EmtpyString
+            // ReSharper disable once UnusedParameter.Local
+            private static void AssertResultEquals(string numbers, int sum)
+            {
+                var stringCalculator = new StringCalculator();
+
+                var result = stringCalculator.Add(numbers);
+
+                Assert.Equal(sum, result);
+            }
+
+            public class EmtpyString : Add
             {
                 [Fact]
                 public void ReturnsZero()
                 {
-                    var stringCalculator = new StringCalculator();
+                    AssertResultEquals(string.Empty, 0);
+                }
+            }
 
-                    var result = stringCalculator.Add(string.Empty);
-
-                    Assert.Equal(0, result);
+            public class SingleNumber
+            {
+                [Theory]
+                [InlineData("0", 0)]
+                [InlineData("1", 1)]
+                [InlineData("2", 2)]
+                public void ReturnsNumber(string numbers, int number)
+                {
+                    AssertResultEquals(numbers, number);
                 }
             }
         }
