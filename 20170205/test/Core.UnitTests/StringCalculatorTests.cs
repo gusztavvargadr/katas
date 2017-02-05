52dd89c88c05850cbf6238edd50140f378111fd5
Gusztáv Varga <me@gusztavvargadr.me>
2017-02-05 13:15:24 +0000

    Add support for validating negative numbers

--- 20170205/test/Core.UnitTests/StringCalculatorTests.cs
+++ 20170205/test/Core.UnitTests/StringCalculatorTests.cs
@@ -75,10 +75,13 @@ namespace TddKata.UnitTests
                 {
                     AssertResultEquals(numbers, sum);
                 }
+            }
 
+            public class MultipleDelimiters : Add
+            {
                 [Theory]
                 [InlineData("1,\n")]
-                public void ThrowsForMultipleDelimiters(string numbers)
+                public void Throws(string numbers)
                 {
                     var stringCalculator = new StringCalculator();
 
@@ -95,6 +98,41 @@ namespace TddKata.UnitTests
                 public void ReturnsSum(string numbers, int sum)
                 {
                     AssertResultEquals(numbers, sum);
+                }
+            }
+
+            public class NegativeNumbers : Add
+            {
+                [Theory]
+                [InlineData("-1")]
+                public void Throws(string numbers)
+                {
+                    var stringCalculator = new StringCalculator();
+
+                    Assert.Throws<ArgumentOutOfRangeException>(() => stringCalculator.Add(numbers));
+                }
+
+                [Theory]
+                [InlineData("-1", new[] {-1})]
+                [InlineData("-1,2", new[] {-1})]
+                [InlineData("-1,-2", new[] {-1, -2})]
+                [InlineData("1,-2,-3", new[] {-2, -3})]
+                [InlineData("-1,-2,-3", new[] {-1, -2, -3})]
+                public void ExceptionMessageContainsNegativeNumbers(string numbers, int[] negativeNumbers)
+                {
+                    try
+                    {
+                        var stringCalculator = new StringCalculator();
+
+                        stringCalculator.Add(numbers);
+
+                        Assert.True(false);
+                    }
+                    catch (ArgumentOutOfRangeException ex)
+                    {
+                        Assert.Contains("negatives not allowed", ex.Message);
+                        Assert.All(negativeNumbers, item => Assert.Contains(item.ToString(), ex.Message));
+                    }
                 }
             }
         }
