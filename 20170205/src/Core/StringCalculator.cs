e223aa5d69d1d05acdb9627423b58c4a246699d5
Gusztáv Varga <me@gusztavvargadr.me>
2017-02-05 11:38:00 +0000

    Add support for emtpy string

--- 20170205/src/Core/StringCalculator.cs
+++ 20170205/src/Core/StringCalculator.cs
@@ -2,5 +2,9 @@
 {
     public class StringCalculator
     {
+        public int Add(string numbers)
+        {
+            return 0;
+        }
     }
 }
--- 20170205/test/Core.UnitTests/StringCalculatorTests.cs
+++ 20170205/test/Core.UnitTests/StringCalculatorTests.cs
@@ -1,6 +1,23 @@
-﻿namespace TddKata.UnitTests
+﻿using Xunit;
+
+namespace TddKata.UnitTests
 {
     public class StringCalculatorTests
     {
+        public class Add
+        {
+            public class EmtpyString
+            {
+                [Fact]
+                public void ReturnsZero()
+                {
+                    var stringCalculator = new StringCalculator();
+
+                    var result = stringCalculator.Add(string.Empty);
+
+                    Assert.Equal(0, result);
+                }
+            }
+        }
     }
 }
