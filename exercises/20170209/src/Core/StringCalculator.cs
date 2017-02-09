8bc88a5caf8ba27b1d9bb634a6745f8b1d13a6e4
Gusztáv Varga <me@gusztavvargadr.me>
2017-02-09 15:01:23 +0000

    Add support for any numbers

--- exercises/20170209/src/Core/StringCalculator.cs
+++ exercises/20170209/src/Core/StringCalculator.cs
@@ -1,20 +1,15 @@
-﻿namespace TddKata
+﻿using System.Linq;
+
+namespace TddKata
 {
     public class StringCalculator
     {
         public int Add(string numbers)
         {
             if (numbers == string.Empty)
-            {
                 return 0;
-            }
 
-            if (numbers.Contains(","))
-            {
-                return int.Parse(numbers[0].ToString()) + int.Parse(numbers[2].ToString());
-            }
-
-            return int.Parse(numbers);
+            return numbers.Split(',').Select(int.Parse).Sum();
         }
     }
 }
--- exercises/20170209/test/Core.UnitTests/StringCalculatorTests.cs
+++ exercises/20170209/test/Core.UnitTests/StringCalculatorTests.cs
@@ -6,16 +6,22 @@ namespace TddKata.UnitTests
     {
         public class Add : StringCalculatorTests
         {
+            // ReSharper disable once UnusedParameter.Local
+            private static void AssertResultEquals(string numbers, int expectedResult)
+            {
+                var stringCalculator = new StringCalculator();
+
+                var result = stringCalculator.Add(numbers);
+
+                Assert.Equal(expectedResult, result);
+            }
+
             public class EmptyString : Add
             {
                 [Fact]
                 public void ReturnsZero()
                 {
-                    var stringCalculator = new StringCalculator();
-
-                    var result = stringCalculator.Add(string.Empty);
-
-                    Assert.Equal(0, result);
+                    AssertResultEquals(string.Empty, 0);
                 }
             }
 
@@ -27,11 +33,7 @@ namespace TddKata.UnitTests
                 [InlineData("2", 2)]
                 public void ReturnsSingleNumber(string numbers, int expectedResult)
                 {
-                    var stringCalculator = new StringCalculator();
-
-                    var result = stringCalculator.Add(numbers);
-
-                    Assert.Equal(expectedResult, result);
+                    AssertResultEquals(numbers, expectedResult);
                 }
             }
 
@@ -43,11 +45,19 @@ namespace TddKata.UnitTests
                 [InlineData("2,3", 5)]
                 public void ReturnsSum(string numbers, int expectedResult)
                 {
-                    var stringCalculator = new StringCalculator();
+                    AssertResultEquals(numbers, expectedResult);
+                }
+            }
 
-                    var result = stringCalculator.Add(numbers);
-
-                    Assert.Equal(expectedResult, result);
+            public class AnyNumbers : Add
+            {
+                [Theory]
+                [InlineData("0,1,2", 3)]
+                [InlineData("1,2,3,4", 10)]
+                [InlineData("2,3,4,5,6", 20)]
+                public void ReturnsSum(string numbers, int expectedResult)
+                {
+                    AssertResultEquals(numbers, expectedResult);
                 }
             }
         }
