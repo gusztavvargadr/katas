3c4776467e1efbe63a59b1c5d013e131ca3c88a6
Gusztáv Varga <me@gusztavvargadr.me>
2017-02-09 15:05:01 +0000

    Add support for newline delimiter

--- exercises/20170209/src/Core/StringCalculator.cs
+++ exercises/20170209/src/Core/StringCalculator.cs
@@ -4,12 +4,14 @@ namespace TddKata
 {
     public class StringCalculator
     {
+        private static readonly char[] Delimiters = {',', '\n'};
+
         public int Add(string numbers)
         {
             if (numbers == string.Empty)
                 return 0;
 
-            return numbers.Split(',').Select(int.Parse).Sum();
+            return numbers.Split(Delimiters).Select(int.Parse).Sum();
         }
     }
 }
--- exercises/20170209/test/Core.UnitTests/StringCalculatorTests.cs
+++ exercises/20170209/test/Core.UnitTests/StringCalculatorTests.cs
@@ -60,6 +60,18 @@ namespace TddKata.UnitTests
                     AssertResultEquals(numbers, expectedResult);
                 }
             }
+
+            public class NewLineDelimiter : Add
+            {
+                [Theory]
+                [InlineData("1\n2,3", 6)]
+                [InlineData("1,2\n3", 6)]
+                [InlineData("1\n2\n3", 6)]
+                public void ReturnsSum(string numbers, int expectedResult)
+                {
+                    AssertResultEquals(numbers, expectedResult);
+                }
+            }
         }
     }
 }
