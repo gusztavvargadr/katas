6652c1ddaec830a7f66c88c5bad8c72ff0861c12
Gusztáv Varga <me@gusztavvargadr.me>
2017-02-10 07:40:34 +0000

    Add skeleton

---
+++ exercises/20170210/src/Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GusztavVargadDr.Tdd.Katas.Core")]
+[assembly: AssemblyDescription("GusztavVargadDr.Tdd.Katas.Core")]
+[assembly: Guid("d384c148-fcee-45a1-8150-be2c416a1f43")]
