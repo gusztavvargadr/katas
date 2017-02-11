6f9e93fdd502643e46382a3a3196e6bc59c32f8a
Gusztáv Varga <me@gusztavvargadr.me>
2017-02-11 09:22:18 +0000

    Add support for any numbers

--- exercises/20170211/src/Core/StringCalculator.cs
+++ exercises/20170211/src/Core/StringCalculator.cs
@@ -1,19 +1,18 @@
-﻿namespace GusztavVargadDr.Tdd.Katas
+﻿using System.Linq;
+
+namespace GusztavVargadDr.Tdd.Katas
 {
     public class StringCalculator
     {
         private const int DefaultSum = 0;
-        private const string NumberDelimiter = ",";
+        private const char NumberDelimiter = ',';
 
         public int Add(string numbers)
         {
             if (numbers == string.Empty)
                 return DefaultSum;
 
-            if (numbers.Contains(NumberDelimiter))
-                return int.Parse(numbers[0].ToString()) + int.Parse(numbers[2].ToString());
-
-            return int.Parse(numbers);
+            return numbers.Split(NumberDelimiter).Sum(int.Parse);
         }
     }
 }
