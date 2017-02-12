326cd9354a182891f14e416c559adeb68ad705eb
Gusztáv Varga <me@gusztavvargadr.me>
2017-02-12 10:01:24 +0000

    Add support for two numbers

--- tdd/string-calculator/sessions/20170212/src/Core/StringCalculator.cs
+++ tdd/string-calculator/sessions/20170212/src/Core/StringCalculator.cs
@@ -1,13 +1,19 @@
+using System.Linq;
+
 namespace GusztavVargadDr.Katas.Tdd
 {
     public class StringCalculator
     {
         private const int DefaultResult = 0;
+        private const char NumberDelimiter = ',';
 
         public int Add(string numbers)
         {
             if (numbers == string.Empty)
                 return DefaultResult;
+
+            if (numbers.Contains(NumberDelimiter))
+                return int.Parse(numbers[0].ToString()) + int.Parse(numbers[2].ToString());
 
             return int.Parse(numbers);
         }
