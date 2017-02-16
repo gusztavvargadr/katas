b4fbbe947eaf8398028437eb7f2914f0385b47e1
Gusztáv Varga <me@gusztavvargadr.me>
2017-02-16 11:47:02 +0000

    Add support for new line delmiter

--- tdd/string-calculator/sessions/20170216/src/Core/StringCalculator.cs
+++ tdd/string-calculator/sessions/20170216/src/Core/StringCalculator.cs
@@ -5,14 +5,14 @@ namespace GusztavVargadDr.Katas.Tdd
     public class StringCalculator
     {
         private const int DefaultSum = 0;
-        private const char NumberDelimiter = ',';
+        private static readonly char[] NumberDelimiters = { ',', '\n' };
 
         public int Add(string numbers)
         {
             if (numbers == string.Empty)
                 return DefaultSum;
 
-            return numbers.Split(NumberDelimiter).Sum(int.Parse);
+            return numbers.Split(NumberDelimiters).Sum(int.Parse);
         }
     }
 }
