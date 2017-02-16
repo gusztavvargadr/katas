6fa1962ce4903cdcd5299ab0906a5b667a35fb36
Gusztáv Varga <me@gusztavvargadr.me>
2017-02-16 12:03:31 +0000

    Add support for validating negative items

--- tdd/string-calculator/sessions/20170216/test/Core.UnitTests/StringCalculatorTests.cs
+++ tdd/string-calculator/sessions/20170216/test/Core.UnitTests/StringCalculatorTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace GusztavVargadDr.Katas.Tdd.UnitTests
 {
@@ -82,6 +83,46 @@ namespace GusztavVargadDr.Katas.Tdd.UnitTests
                 public void ReturnsSum(string numbers, int sum)
                 {
                     AssertResultEquals(numbers, sum);
+                }
+            }
+
+            public class NegativeNumber : Add
+            {
+                [Theory]
+                [InlineData("-1")]
+                [InlineData("2,-3")]
+                [InlineData("-3,-4")]
+                public void ThrowsArgumentOutOfRangeException(string numbers)
+                {
+                    var stringCalculator = new StringCalculator();
+
+                    Assert.Throws<ArgumentOutOfRangeException>(() => stringCalculator.Add(numbers));
+                }
+
+                [Theory]
+                [InlineData("-1")]
+                [InlineData("2,-3")]
+                [InlineData("-3,-4")]
+                public void ExceptionMessageContainsNegativesNotAllowedMessage(string numbers)
+                {
+                    var stringCalculator = new StringCalculator();
+
+                    var ex = Assert.ThrowsAny<Exception>(() => stringCalculator.Add(numbers));
+
+                    Assert.Contains("negatives not allowed", ex.Message);
+                }
+
+                [Theory]
+                [InlineData("-1", new[] {-1})]
+                [InlineData("2,-3", new[] {-3})]
+                [InlineData("-3,-4", new[] {-3, -4})]
+                public void ExceptionMessageContainsNegativeNumbers(string numbers, int[] negativeNumbers)
+                {
+                    var stringCalculator = new StringCalculator();
+
+                    var ex = Assert.ThrowsAny<Exception>(() => stringCalculator.Add(numbers));
+
+                    Assert.All(negativeNumbers, item => Assert.Contains(item.ToString(), ex.Message));
                 }
             }
         }
