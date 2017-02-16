6699cf1d58ded497da6ed698a768158451a92342
Gusztáv Varga <me@gusztavvargadr.me>
2017-02-16 11:44:23 +0000

    Add support for multiple numbers

--- tdd/string-calculator/sessions/20170216/src/Core/StringCalculator.cs
+++ tdd/string-calculator/sessions/20170216/src/Core/StringCalculator.cs
@@ -12,10 +12,7 @@ namespace GusztavVargadDr.Katas.Tdd
             if (numbers == string.Empty)
                 return DefaultSum;
 
-            if (numbers.Contains(NumberDelimiter))
-                return int.Parse(numbers[0].ToString()) + int.Parse(numbers[2].ToString());
-
-            return int.Parse(numbers);
+            return numbers.Split(NumberDelimiter).Sum(int.Parse);
         }
     }
 }
