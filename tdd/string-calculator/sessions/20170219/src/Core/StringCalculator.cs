6dbf23c6fede997a4bb52f36a55644f380fba310
Gusztáv Varga <me@gusztavvargadr.me>
2017-02-19 09:02:45 +0000

    Add support for new line delimiter

--- tdd/string-calculator/sessions/20170219/src/Core/StringCalculator.cs
+++ tdd/string-calculator/sessions/20170219/src/Core/StringCalculator.cs
@@ -5,17 +5,16 @@ namespace GusztavVargadDr.Katas.Tdd
     public class StringCalculator
     {
         private const int DefaultResult = 0;
-        private const char Delimiter = ',';
+
+        private static readonly char[] Delimiters = {',', '\n'};
+
 
         public int Add(string numbers)
         {
             if (numbers == string.Empty)
                 return DefaultResult;
 
-            if (numbers.Contains(Delimiter))
-                return numbers.Split(Delimiter).Sum(int.Parse);
-
-            return int.Parse(numbers);
+            return numbers.Split(Delimiters).Sum(int.Parse);
         }
     }
 }
