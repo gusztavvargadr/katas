b17e0bc3e8bb4a6956fdb876c297e75de07c4344
Gusztáv Varga <me@gusztavvargadr.me>
2017-02-19 09:07:14 +0000

    Add support for custom delimiters

--- tdd/string-calculator/sessions/20170219/src/Core/StringCalculator.cs
+++ tdd/string-calculator/sessions/20170219/src/Core/StringCalculator.cs
@@ -6,13 +6,28 @@ namespace GusztavVargadDr.Katas.Tdd
     {
         private const int DefaultResult = 0;
 
-        private static readonly char[] Delimiters = {',', '\n'};
+        private const string CustomDelimiterMark = "//";
+        private const char CustomDelimiterDelimiter = '\n';
 
+        private static readonly char[] DefaultDelimiters = {',', '\n'};
+
+        public StringCalculator()
+        {
+            Delimiters = DefaultDelimiters;
+        }
+
+        private char[] Delimiters { get; set; }
 
         public int Add(string numbers)
         {
             if (numbers == string.Empty)
                 return DefaultResult;
+
+            if (numbers.StartsWith(CustomDelimiterMark) && numbers[3] == CustomDelimiterDelimiter)
+            {
+                Delimiters = new[] {numbers[2]};
+                numbers = numbers.Substring(4);
+            }
 
             return numbers.Split(Delimiters).Sum(int.Parse);
         }
--- tdd/string-calculator/sessions/20170219/test/Core.UnitTests/StringCalculatorTests.cs
+++ tdd/string-calculator/sessions/20170219/test/Core.UnitTests/StringCalculatorTests.cs
@@ -72,6 +72,18 @@ namespace GusztavVargadDr.Katas.Tdd.UnitTests
                     AssertResultEquals(numbers, sum);
                 }
             }
+
+            public class CustomDelimiter : Add
+            {
+                [Theory]
+                [InlineData("//:\n0:1", 1)]
+                [InlineData("//;\n1;2", 3)]
+                [InlineData("///\n2/3", 5)]
+                public void ReturnsSum(string numbers, int sum)
+                {
+                    AssertResultEquals(numbers, sum);
+                }
+            }
         }
     }
 }
