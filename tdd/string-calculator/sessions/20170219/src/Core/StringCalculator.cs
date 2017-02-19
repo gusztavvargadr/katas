9649b9bc8141a77f09e6322401bce8e3d26babc3
Gusztáv Varga <me@gusztavvargadr.me>
2017-02-19 08:45:07 +0000

    Add support for empty string

---
+++ tdd/string-calculator/sessions/20170219/src/Core/StringCalculator.cs
@@ -0,0 +1,12 @@
+namespace GusztavVargadDr.Katas.Tdd
+{
+    public class StringCalculator
+    {
+        private const int DefaultResult = 0;
+
+        public int Add(string numbers)
+        {
+            return DefaultResult;
+        }
+    }
+}
---
+++ tdd/string-calculator/sessions/20170219/test/Core.UnitTests/StringCalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace GusztavVargadDr.Katas.Tdd.UnitTests
+{
+    public class StringCalculatorTests
+    {
+        public class Add : StringCalculatorTests
+        {
+            public class EmptyString : Add
+            {
+                [Fact]
+                public void RetunsZero()
+                {
+                    var stringCalculator = new StringCalculator();
+
+                    var result = stringCalculator.Add(string.Empty);
+
+                    Assert.Equal(0, result);
+                }
+            }
+        }
+    }
+}
