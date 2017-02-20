2b973e8c7eac206e2b718fa3584799b2bde99304
Gusztáv Varga <me@gusztavvargadr.me>
2017-02-20 20:33:14 +0000

    Add support for empty string

---
+++ tdd/string-calculator/sessions/20170220/src/Core/StringCalculator.cs
@@ -0,0 +1,12 @@
+namespace GusztavVargadDr.Katas.Tdd
+{
+    public class StringCalculator
+    {
+        private const int DefaultSum = 0;
+
+        public int Add(string numbers)
+        {
+            return DefaultSum;
+        }
+    }
+}
