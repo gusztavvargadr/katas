b0d01f9dfce658e483a1799053f99cbeea8b405f
Gusztáv Varga <me@gusztavvargadr.me>
2017-02-20 20:26:38 +0000

    Add skeleton

---
+++ tdd/string-calculator/sessions/20170220/src/Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GusztavVargadDr.Katas.Tdd.Core")]
+[assembly: AssemblyDescription("GusztavVargadDr.Katas.Tdd.Core")]
+[assembly: Guid("d384c148-fcee-45a1-8150-be2c416a1f43")]
